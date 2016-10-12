470cef614b4ef800c3f01c232b88874103bcb948
Eugene Maslenkov <maslenkov@i.ua>
2016-10-12 17:51:35 +0300

    fix data structure

--- web/models/tags.ex
+++ web/models/tags.ex
@@ -1,14 +1,13 @@
 defmodule HelloPhoenix.Tags do
   use HelloPhoenix.Web, :model
 
+  @primary_key {:key, :string, autogenerate: false}
   embedded_schema do
     field :values, {:array, :string}
   end
 
-  # def changeset(struct, params \\ %{}) do
-  #   struct
-  #   |> cast(params, [:name])
-  #   |> cast_embed(:tags)
-  #   |> validate_required([:name])
-  # end
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:key, :values])
+  end
 end
