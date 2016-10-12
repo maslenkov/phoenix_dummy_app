86c9d4a865e2467f5a9b664b509e9b36e903ffe3
Eugene Maslenkov <maslenkov@i.ua>
2016-10-12 21:30:22 +0300

    emebds deleting realisations

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -1,9 +1,9 @@
 defmodule HelloPhoenix.UserTest do
   use HelloPhoenix.ModelCase
 
-  alias HelloPhoenix.User
+  alias HelloPhoenix.{User, Repo}
 
-  @valid_attrs %{name: "some content", tags: [%{values: ["a", "b"]}]}
+  @valid_attrs %{name: "some content", tags: [%{key: "some content", values: ["a", "b"]}]}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -14,5 +14,49 @@ defmodule HelloPhoenix.UserTest do
   test "changeset with invalid attributes" do
     changeset = User.changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  describe "embeds_many" do
+    test "doc way" do
+      {:ok, user} = %User{}
+        |> User.changeset(%{name: "Eugene", tags: [%{key: "a", values: ["1", "2"]}, %{key: "b", values: ["3"]}]})
+        |> Repo.insert
+
+      assert Enum.count(user.tags) == 2
+
+      {status, user} = user
+        |> User.changeset(%{tags: []})
+        |> Repo.update
+
+      if status == :error do
+        IO.inspect user.errors
+      end
+      assert user.tags == []
+    end
+
+    test "platformatec way" do
+      {:ok, user} = %User{}
+        |> User.changeset(%{name: "Eugene", tags: [%{key: "a", values: ["1", "2"]}, %{key: "b", values: ["3"]}]})
+        |> Repo.insert
+
+      assert Enum.count(user.tags) == 2
+
+      map_with_tag_marked_to_delete = user.tags
+        |> Enum.map(fn (tag) ->
+          if tag.key == "a" do
+            %{tag | delete: true}
+          else
+            tag
+          end |> Map.from_struct
+        end)
+      {status, user} = user
+        |> User.changeset(%{tags: map_with_tag_marked_to_delete})
+        |> Repo.update
+
+      if status == :error do
+        IO.inspect user.errors
+      end
+      assert Enum.count(user.tags) == 1
+    end
   end
 end
--- web/models/tags.ex
+++ web/models/tags.ex
@@ -4,10 +4,21 @@ defmodule HelloPhoenix.Tags do
   @primary_key {:key, :string, autogenerate: false}
   embedded_schema do
     field :values, {:array, :string}
+    field :delete, :boolean, virtual: true
   end
 
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:key, :values])
+    |> validate_required([:key])
+    |> mark_for_deletion()
+  end
+
+  defp mark_for_deletion(changeset) do
+    if get_change(changeset, :delete) do
+      %{changeset | action: :delete}
+    else
+      changeset
+    end
   end
 end
